055316f1bf0092f24b64406e29d54cb525216555
Peem <mrptvi@gmail.com>
2025-04-19 23:33:09 +0700

    feat: route delete animation

--- routes/animationRoute.js
+++ routes/animationRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const {getAllAnimations, createAnimation, updateAnimation} = require("../controllers/animationController");
+const {getAllAnimations, createAnimation, updateAnimation, deleteAnimation} = require("../controllers/animationController");
 const {protect} = require("../middleware/auth");
 const router = express.Router();
 
@@ -10,5 +10,6 @@ router.post("/", protect, createAnimation);
 
 // new route to upload one image (base64) and append it
 router.put("/:id", protect, updateAnimation);
+router.delete("/:id", protect, deleteAnimation);
 
 module.exports = router;
